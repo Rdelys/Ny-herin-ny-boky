86ef597cb98bd6177db51fc1ba5a227a255ef265
Rdelys <rdauphinelys@gmail.com>
2026-09-18 15:14:20 +0300

    api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->alias([
            'admin' => \App\Http\Middleware\EnsureAdmin::class,
+            'client.api' => \App\Http\Middleware\EnsureClientApi::class,
        ]);
 
     })
